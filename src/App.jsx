7829d141adb1cabda87f37e3597ad1771f4c8379
nk559 <nareshkumaranandan77@gmail.com>
2025-12-11 22:23:01 +0530

    Updated UI layout

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React I am the best yyyy
-        
+      <h1>Vite + React I am the best yyyyss
+
       </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
